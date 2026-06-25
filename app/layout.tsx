3ecc4e556154cd27480ce8a54aaaf76edc9d9bfa
Dawud Satrio Wicaksono <dawudsw0491@gmail.com>
2026-06-25 15:55:52 +0700

    style: improve application metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,18 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Strategi Pengembangan Pasar | PT Sakti Setia Sentosa",
   description:
-    "Website Presentasi Strategi Pengembangan Pasar PT Sakti Setia Sentosa",
+    "Website presentasi strategi pengembangan pasar PT Sakti Setia Sentosa.",
+  keywords: [
+    "Strategi Pengembangan Pasar",
+    "Market Development",
+    "Presentasi",
+    "PT Sakti Setia Sentosa",
+  ],
+  authors: [
+    {
+      name: "Dawud",
+    },
+  ],
 };
 
 interface RootLayoutProps {
@@ -17,8 +28,11 @@ export default function RootLayout({
   children,
 }: RootLayoutProps) {
   return (
-    <html lang="id" suppressHydrationWarning>
-      <body className="bg-slate-50 text-slate-800 antialiased">
+    <html
+      lang="id"
+      suppressHydrationWarning
+    >
+      <body className="min-h-screen bg-slate-50 text-slate-800 antialiased">
         {children}
       </body>
     </html>
