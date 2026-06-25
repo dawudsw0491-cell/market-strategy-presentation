54bb476fa74c002bd657386563fe895766dd697a
Dawud Satrio Wicaksono <dawudsw0491@gmail.com>
2026-06-25 16:11:53 +0700

    style: improve responsive container spacing

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -15,9 +15,10 @@ export default function Container({
         mx-auto
         w-full
         max-w-7xl
-        px-6
-        sm:px-8
+        px-5
+        sm:px-6
         lg:px-10
+        xl:px-12
         ${className}
       `}
     >
